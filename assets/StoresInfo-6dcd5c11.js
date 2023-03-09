9465824c84f17b50a7f9ac30a2352ba83cd3a621
sasimi1327 <wonderland1327@gmail.com>
2023-03-09 21:19:23 +0800

    deploy

---
+++ assets/StoresInfo-6dcd5c11.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-bfa95f3f.js";const t={},n={class:"container"};function s(r,_){return c(),o("div",n," 店鋪資訊 ")}const f=e(t,[["render",s]]);export{f as default};
